c297646dbeb9b411a962bfda5911c3d83fa6ce7f
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-02-06 01:25:58 -0400

    Add user inputs for name, age, score, and active status

--- testInputs.py
+++ testInputs.py
@@ -3,3 +3,13 @@
 result = input('Ingresa tu nombre: ')
 print('Se ingresó por teclado: ' + result)
 print(type(result))
+
+name = input('Ingresa tu nombre: ')
+age = input('Ingresa tu edad: ')
+score = input('Ingresa tu calificación: ')
+active = input('El usuario se encuentra activo?: ')
+
+print('Nombre: ' + name)
+print('Edad: ' + age)
+print('Calificación: ' + score)
+print('Activo: ' + active)
