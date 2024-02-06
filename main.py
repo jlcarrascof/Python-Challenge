48dbc66670d0b65aeadfe55f5ac1395d9271596a
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-02-05 22:18:21 -0400

    Adds variables for last name and full name, and prints them to the console

--- main.py
+++ main.py
@@ -3,4 +3,10 @@
 
 # nombre_de_la_variable = valor
 first_name = "Eduardo Ismael" # String
+last_name = "García Pérez" 
 print(first_name)
+print(last_name)
+
+full_name = first_name + ' ' + last_name
+print(full_name)
+
