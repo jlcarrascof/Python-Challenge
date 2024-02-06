b638c006866339619ee184a57859fa8c0c92e6f0
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-02-06 19:26:57 -0400

    Add control structures and operators to number comparison

---
+++ control-structures.py
@@ -0,0 +1,14 @@
+# Estructuras de control.
+# if, match (switch), while, foreach, while.
+
+# Bool
+# True, False
+
+# Operadores relacionales (==, !=, <, >, <=, >=)
+# Operadores lógicos (and, or, not)
+
+number1 = 10
+number2 = 20
+
+result = number1 == number2
+print(result)
