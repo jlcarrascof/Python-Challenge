14ff23b0fda77cb0eda0dc42a8a221de4589adce
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-02-07 10:12:24 -0400

    Add foreach loop examples for iterating over characters and numbers

---
+++ loops.py
@@ -0,0 +1,15 @@
+# foreach y while
+
+# foreach cuando sepamos cuantas iteraciones vamos a hacer
+# while cuando no sepamos cuantas iteraciones vamos a hacer
+
+# foreach
+
+title = 'Estructuras de control'
+for caracter in title:
+    print(caracter)
+
+for number in range(1, 101):
+    if number % 2 == 0:
+        print(number)
+
