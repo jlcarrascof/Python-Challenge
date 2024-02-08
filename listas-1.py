dcf197d6cca6a2cd890b772ab410d902fe9a8a00
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-02-08 17:49:36 -0400

    Add elements to courses list using append

--- listas-1.py
+++ listas-1.py
@@ -1,8 +1,13 @@
 # Listas - Estructuras de Datos.
+# append, insert, extend, remove, clear, index, count.
 
 # Indices      0        1        2        3        4               5      6
 #                                         -4       -3             -2      -1  
 courses = ['Python', 'Django', 'Flask', 'Ruby', 'Ruby on Rails', 'Rust', 'C#']
 
-new_list = courses[::-1] 
-print(new_list)
+courses.append('JavaScript')
+courses.append('TypeScript')
+courses.append('Swift')
+
+print(courses)
+print(courses[-1])
