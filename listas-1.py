36d75a42c699d9d98d5d31a4d7ce1e76cd8486e6
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-02-08 17:38:57 -0400

    Add example of full list copy using slicing

--- listas-1.py
+++ listas-1.py
@@ -4,5 +4,5 @@
 #                                         -4       -3             -2      -1  
 courses = ['Python', 'Django', 'Flask', 'Ruby', 'Ruby on Rails', 'Rust', 'C#']
 
-new_list = courses[3:] 
+new_list = courses[:] 
 print(new_list)
