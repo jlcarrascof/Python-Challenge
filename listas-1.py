267936dedac469a5e40c65d9d53be3904d3c1572
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-02-08 18:05:00 -0400

    Extend the courses list with 'Java9', 'Docker', and 'Unix'

--- listas-1.py
+++ listas-1.py
@@ -4,12 +4,9 @@
 # Indices      0        1        2        3        4               5      6
 #                                         -4       -3             -2      -1  
 courses = ['Python', 'Django', 'Flask', 'Ruby', 'Ruby on Rails', 'Rust', 'C#']
+new_courses = ['Java9', 'Docker', 'Unix']
 
-courses.append('JavaScript')
-courses.append('TypeScript')
-courses.append('Swift')
-
-courses.insert(1, 'SQL Server')
+courses.extend(new_courses)
 
 print(courses)
 print(courses[-1])
