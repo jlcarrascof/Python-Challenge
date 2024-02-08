03ce26ec872b83626f052255fc00a162d8b4b8f5
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-02-08 11:07:32 -0400

    Add list slicing example

--- listas-1.py
+++ listas-1.py
@@ -1,12 +1,8 @@
 # Listas - Estructuras de Datos.
 
 # Indices      0        1        2        3        4               5      6
+#                                         -4       -3             -2      -1  
 courses = ['Python', 'Django', 'Flask', 'Ruby', 'Ruby on Rails', 'Rust', 'C#']
 
-last_index = len(courses) - 1
-index = int(input('Ingrese el indice del curso que desea ver: '))
-
-if index <= last_index:
-    print(courses[index])
-else:
-    print('Lo sentimos, el índice ingresado NO es válido')
+new_list = courses[0:5] 
+print(new_list)
