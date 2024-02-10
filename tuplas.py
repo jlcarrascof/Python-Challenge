f0bc7e8c8b9b57a9647ee1b9013547ed07f72305
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-02-10 13:40:39 -0400

    Unpack the database settings tuple into individual variables and print them

--- tuplas.py
+++ tuplas.py
@@ -2,5 +2,10 @@
 
 settings = ('localhost', 3306, 'root', 'password', 'database')
 
-for setting in settings:
-    print(setting)
+host = settings[0]
+port = settings[1]
+username = settings[2]
+password = settings[3]
+name = settings[4]
+
+print(host, port, username, password, name)
