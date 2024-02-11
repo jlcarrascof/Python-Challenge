6a5b7b1b9b422ac3d64f85eb269af9a4b77721a0
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-02-10 20:03:04 -0400

    Add a dictionary for a user and print the user's active status

--- tuplas.py
+++ tuplas.py
@@ -1,13 +1,10 @@
 # Tuplas - Diccionarios.
 
-settings = ('localhost', 3306, 'root', 'password', 'database')
+user = {
+    'name': 'Cody',
+    'age': 10,
+    'email': 'info@codigofacilito.com',
+    'active': True
+}
 
-tuples = (
-    (1, 2, 3),
-    (4, 5, 6),
-    (7, 8, 9)
-)
-
-for number1, number2, number3 in tuples:
-    print(number1, number2, number3)
-    
+print(user['active'])
